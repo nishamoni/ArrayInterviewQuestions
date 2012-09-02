39f3781207a779d9de620ba400f89c7374676086
Kadavi <coedry@gmail.com>
2012-09-01 23:56:55 -0700

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,23 +1,18 @@
 public class Factorial {
     
     public static int factorial(int num) {
-        if(num <= 1) {
+        if(num <= 1)
             return 1;
-        }
         
         return num * factorial(num-1);
         
         /*
         // Comment out above then uncomment this for non-recursive style 
-        for( int i = num ; i > 1 ; i-- ) {
-            
+        for( int i = num ; i > 1 ; i-- )  
             num *= i-1;        
-            
-        }
 
         return num;
         */
-    
     }
 
     public static void main(String[] args) {
